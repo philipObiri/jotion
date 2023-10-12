4e337a55b36fc6ca3c67f36a3090f286b893c63c
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-12 12:00:07 +0000

    Initializing  Project

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
+export default function Home() {
+  return ( 
+   <div>
+    <Button className="bg-emerald-500 text-white" size="sm">
+      Click Me
+    </Button>
+   </div>
+   )
+}
